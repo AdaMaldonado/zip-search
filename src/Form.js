41398b2a9e96b8902fdc790053ed6fb0bb1fb0ac
AdaMaldonado <34352705+AdaMaldonado@users.noreply.github.com>
2022-01-12 15:48:44 -0500

    Working project

---
+++ src/Form.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+export default function Form(props) {
+
+    const [zipCode, setZipCode] = useState("")
+
+    const getZip = () => {
+        axios.get(`http://ctp-zip-api.herokuapp.com/zip/${zipCode}`)
+            .then(response => {props.setData(response.data)})
+    }
+
+    return (
+        <div>
+            <h1>Zip Code Search</h1>
+            <label>Zip Code:</label>
+            <input placeholder="Try 10016"
+                   onChange={event => setZipCode(event.target.value)}
+                   value={zipCode}/>
+            <button onClick={getZip}>Get Details</button>
+        </div>
+    )
+}
