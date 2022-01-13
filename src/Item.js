1fb15fe03110fa71f93e35e0a0522a6e0edaeee0
AdaMaldonado <34352705+AdaMaldonado@users.noreply.github.com>
2022-01-12 21:44:53 -0500

    Project completed

--- src/Item.js
+++ src/Item.js
@@ -5,16 +5,18 @@ export default function Item(props){
     console.log(props.data)
 
     return (
-        <div>
-            <h2>
-                State: {props.data.State}
-                <br></br>
-                Location: ({props.data.Lat}, {props.data.Long})
-                <br></br>
-                Population (estimated): {props.data.EstimatedPopulation}
-                <br></br>
-                Total Wage: {props.data.TotalWages}
-            </h2>
+        <div id="box">
+            <div id="data">
+                <div id="head">
+                    <p>{props.data.City}, {props.data.State}</p>
+                </div>
+                    <ul>
+                        <li>State: {props.data.State}</li>
+                        <li>Location: ({props.data.Lat}, {props.data.Long})</li>
+                        <li>Population (estimated): {props.data.EstimatedPopulation}</li>
+                        <li>Total Wage: {props.data.TotalWages}</li>
+                    </ul>
+            </div>
         </div>
     )
 }
